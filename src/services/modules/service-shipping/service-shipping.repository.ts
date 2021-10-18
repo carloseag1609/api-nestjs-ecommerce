1925fb32ac63583d879e3d723f184b0ef32e39e7
CarlosEAG1609 <carloseag1233@gmail.com>
2021-10-18 10:49:02 -0500

    Add repository method to create a new shipping

--- src/services/modules/service-shipping/service-shipping.repository.ts
+++ src/services/modules/service-shipping/service-shipping.repository.ts
@@ -1,5 +1,21 @@
+import { Region } from 'src/addresses/modules/regions/entities/region.entity';
+import { Service } from 'src/services/entities/service.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { ServiceShipping } from './entities/service-shipping.entity';
 
 @EntityRepository(ServiceShipping)
-export class ServiceShippingRepository extends Repository<ServiceShipping> {}
+export class ServiceShippingRepository extends Repository<ServiceShipping> {
+  async createServiceShipping(
+    service: Service,
+    region: Region,
+    price: number,
+  ): Promise<ServiceShipping> {
+    const shipping = await this.create({
+      service,
+      region,
+      price,
+    });
+    await this.save(shipping);
+    return shipping;
+  }
+}
